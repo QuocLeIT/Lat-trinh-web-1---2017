e18c7635c94af55a82632efb0883b4f77afeee99
QuocLeIT <quoc.le.2107@gmail.com>
2017-10-22 16:26:54 +0700

    Update BTVN06

--- login-logout-register/functions.php
+++ login-logout-register/functions.php
@@ -19,3 +19,28 @@ function createUser($fullname, $email, $password) {
   $stmt->execute(array($email, $password, $fullname));
   return $db->lastInsertId();
 }
+function createPosts($id, $userId, $content) {
+  global $db;
+  $stmt = $db->prepare("INSERT INTO posts (id, userId, content) VALUE (?, ?, ?)");
+  $stmt->execute(array($id, $userId, $content));
+  return $db->lastInsertId();
+}
+function updatePassword($password, $id) {
+  global $db;
+  $stmt = $db->prepare("UPDATE users SET password = ? where id = ?");
+  $stmt->execute(array($password, $id));
+  return $id;
+}
+function updateThongTinCaNhan($fullname, $phone, $id) {
+  global $db;
+  $stmt = $db->prepare("UPDATE users SET fullname = ?, phone = ? WHERE id = ?");
+  $stmt->execute(array($fullname, $phone, $id));
+  return $id;
+}
+function readCaption() {
+  global $db;
+  $stmt = $db->query("SELECT users.fullname, posts.content, posts.createdAt FROM users, posts WHERE users.id = posts.id");
+  //$stmt->execute(array(strtolower($email)));
+  $caption = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $caption;
+}
